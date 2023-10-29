68abed35b109ec1330a1085aa20cc9454157893e
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-10-29 21:05:04 +0000

    Deploying to gh-pages from @ Volodymyr123rusofob/goit-js-hw-08_26@e6c0d56fac79bfedd2577bfeebd2343f0435775d 🚀

---
+++ 03-feedback.726ad425.js
@@ -0,0 +1,2 @@
+document.querySelector("form").addEventListener("input",saveValue);
+//# sourceMappingURL=03-feedback.726ad425.js.map
---
+++ 03-feedback.bf832293.js
@@ -0,0 +1,2 @@
+document.querySelector("form").addEventListener("input",saveValue);
+//# sourceMappingURL=03-feedback.bf832293.js.map
