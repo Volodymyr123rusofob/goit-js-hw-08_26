9e17857544f08388975e9506b3c12f68a53c2946
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-10-31 00:40:50 +0200

    taska-0-0

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,31 @@
-const formLink = document.querySelector('form');
-formLink.addEventListener('input', saveValue);
+import throttle from 'lodash.throttle';
+const refs = {
+  form: document.querySelector('form'),
+  textarea: document.querySelector('textarea'),
+  email: document.querySelector('[name="email"]'),
+};
+
+const saveFormValue = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+chekForm();
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+function onTextareaInput(event) {
+  saveFormValue[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(saveFormValue));
+}
+
+function chekForm() {
+  const saveMessage = localStorage.getItem('feedback-form-state');
+  if (saveMessage) {
+    const valueFormInput = JSON.parse(saveMessage);
+    refs.textarea.value = valueFormInput.message;
+    refs.email.value = valueFormInput.email;
+  }
+}
